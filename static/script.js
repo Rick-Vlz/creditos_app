d19200068569d7ff7d6cab7e5365eedc5c7e0675
Rick Valdes <ricardovlz.wlf@gmail.com>
2025-04-18 18:51:10 -0600

    Update v0.0.2

--- static/script.js
+++ static/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let editId = null;
     let chart = null;
 
-    const crearOActualizarGrafico = (labels, montos) => {
+    const datosGrafico = (labels, montos) => {
         if (chart) {
             // Comprobar datos
             chart.data.labels = labels;
@@ -15,15 +15,15 @@ document.addEventListener("DOMContentLoaded", () => {
         } else {
             // Añadir y crear en chart si no hay datos
             chart = new Chart(ctx, {
-                type: 'doughnut',
-                data: {
+                type: 'doughnut', //Tipo de grafica de dona
+                data: {//Datos de la grafica
                     labels: labels,
                     datasets: [{
                         label: 'Monto Total',
                         data: montos
                     }]
                 },
-                options: {
+                options: {//Opciones de la grafica
                     responsive: true,
                     maintainAspectRatio: false,
                     plugins: {
@@ -39,11 +39,11 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     };
 
-    const cargarCreditos = async () => {
+    const cargarCreditos = async () => {//Se llama a la API GET para obetener los datos
         const res = await fetch("/api/creditos");
         const data = await res.json();
         tabla.innerHTML = "";
-        data.forEach(c => {
+        data.forEach(c => {//Se llena la tabla HTML con los datos
             tabla.innerHTML += `
                 <tr>
                     <td>${c.cliente}</td>
@@ -58,15 +58,15 @@ document.addEventListener("DOMContentLoaded", () => {
                 </tr>
             `;
         });
-        const agrupado = data.reduce((acc, c) => {
-            acc[c.cliente] = (acc[c.cliente] || 0) + parseFloat(c.monto);
+        const agrupado = data.reduce((acc, c) => {//Se otiene los nombres de clientes para las labels
+            acc[c.cliente] = (acc[c.cliente] || 0) + parseFloat(c.monto);//Se hace un arreglo de clientes y sus montos
             return acc;
         }, {});
     
         const labels = Object.keys(agrupado);
         const montos = labels.map(cliente => agrupado[cliente]);
 
-        crearOActualizarGrafico(labels, montos);
+        datosGrafico(labels, montos); //Se pasan a la funcion para graficarse los datos
     };
 
     form.addEventListener("submit", async (e) => {
@@ -74,7 +74,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const formData = new FormData(form);
         const jsonData = Object.fromEntries(formData.entries());
     
-        if (editId) {
+        if (editId) {//Detecta si existe en el arreglo de guardado un id, lo cual indica que se trata de una actualizacion
             await fetch(`/api/creditos/${editId}`, {
                 method: "PUT",
                 headers: { "Content-Type": "application/json" },
@@ -82,7 +82,7 @@ document.addEventListener("DOMContentLoaded", () => {
             });
             editId = null;
             form.querySelector("button[type='submit']").textContent = "Guardar";
-        } else {
+        } else {//Trata la llamada a la api como un POST, osea un nuevo registro
             await fetch("/api/creditos", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
@@ -91,23 +91,23 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     
         form.reset();
-        cargarCreditos();
+        cargarCreditos();//Actualiza la tabla
     });
     
 
-    window.eliminar = async (id) => {
+    window.eliminar = async (id) => {//Llama a la API delete para eliminar el resgitro usando el id de este
         await fetch(`/api/creditos/${id}`, { method: "DELETE" });
-        cargarCreditos();
+        cargarCreditos();//Actualiza la tabla
     };
 
     window.editar = (id, cliente, monto, interes, plazo, fecha) => {
-        form.cliente.value = cliente;
+        form.cliente.value = cliente;//Añade en el form los datos del registro a actualizar
         form.monto.value = monto;
         form.tasa_interes.value = interes;
         form.plazo.value = plazo;
         form.fecha_otorgamiento.value = fecha;
-        editId = id;
+        editId = id;//ingresa el id para confirmar una edicion y no un nuevo registro
     };    
 
-    cargarCreditos();
+    cargarCreditos();//Actualizar tabla
 });
